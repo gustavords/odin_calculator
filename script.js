94f745033f8640effd307dd82ddb8383d24f6545
gustavords <gustavords92@gmail.com>
2022-07-05 17:58:33 -0400

    Fix add, sub, equal, a/c and del functions

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 const display = document.querySelector(`.cal-display-item`);
 const buttons = document.querySelectorAll(`button`);
 const numBtns = document.querySelectorAll(`.num`);
-let text = ``;
 
+let currentValue = ``;
+let buttonValuePressed = ``;
+let displayText = ``;
+let valueA = 0;
+
+let equalWasPressed = false;
+
+//eventlistener for the number buttons
 numBtns.forEach((button) => {
   button.addEventListener(`click`, (e) => {
-    let displayText = e.target.textContent; //what is actually pressed
-    console.log(`displayText: ${displayText}`);
+    //clears screen after each press
+    display.textContent = ``;
 
-    // takes in current display
-    text += displayText;
-    console.log(`text: ${text}`);
+    buttonValuePressed = e.target.textContent; //the value of what is actually pressed
+    console.log(`buttonValuePressed:${buttonValuePressed}`);
+
+    // takes in current button pressed and adds to string
+    currentValue += buttonValuePressed;
+    console.log(`currentValue: ${currentValue}`);
+
+    displayText = currentValue; //WILL GO to display
+    console.log(`displayText: ${displayText}`);
 
     //remove display text
     if (display.textContent === `Display`) {
@@ -18,7 +31,7 @@ numBtns.forEach((button) => {
     }
 
     //limit numbers entered
-    if (text.length > 9) {
+    if (currentValue.length > 9) {
       buttons.disabled = true;
 
       //blocks buttons
@@ -26,79 +39,146 @@ numBtns.forEach((button) => {
 
       console.log(`worked`);
     } else {
-      display.textContent += displayText;
+      display.textContent = displayText;
     }
 
-    console.log(`text.length: ${text.length}`);
+    console.log(`currentValue.length: ${currentValue.length}`);
     console.log(`displayText.length: ${displayText.length}`);
   });
 });
 
+console.log(`currentValue: ${currentValue}`);
+
+//event listener for operator buttons
 buttons.forEach((button) => {
   button.addEventListener(`click`, (e) => {
     // console.log(e.target);
-    //Delete Button
-    if (e.target.textContent === `Del`) delBtn();
-    //A/C Button
-    if (e.target.textContent === `A/C`) acBtn();
 
-    if (e.target.textContent === `+`) addBtn();
-
-    if (e.target.textContent === `=`) {
-      equalBtnPressed();
+    //TODO:turn into a switch
+    switch (e.target.textContent) {
+      case `Del`:
+        delBtn();
+        break;
+      case `A/C`:
+        acBtn();
+        break;
+      case `+`:
+        add();
+        break;
+      case "-":
+        sub();
+        break;
+      case "=":
+        equals();
+        break;
+      default:
+        console.log(`e.target.textContent:${e.target.textContent} was pressed`);
     }
+
+    // if (e.target.textContent === `=`) {
+    //   console.log(`equalWasPressed:${(equalWasPressed = true)} `);
+    //   equals();
+    //   console.log(`equalWasPressed:${equalWasPressed}`);
+    //   // equalBtnPressed();
+    // }
   });
 });
 
-let addBtnPressed = false;
-let calcArr = [];
-let currentCalc;
-
-function addBtn() {
-  //erases display
-  text = display.textContent;
-  console.log(text);
-  console.log(display.textContent);
-  console.log(currentCalc);
-
-  display.textContent = ` `;
-  display.textContent = ` `;
-
-  //cahnge background colour to notice its pressed
-  document.querySelector(
-    `.cal-plus-item`
-  ).style.cssText = `background-color: var(--grey-o);`;
-
-  //converst text to num
-  text = +text;
-
-  //tells add-button is pressed
-  addBtnPressed = true;
-
-  // console.log(`to num: ${typeof text} text: ${text}`);
-
-  calcArr.push(text);
-  text = ``;
-  console.log(calcArr);
-  if (calcArr.length === 2) {
-    display.textContent = ``;
-    currentCalc = `${calcArr[0] + calcArr[1]}`;
-
-    console.log(`currentCalc: ${currentCalc}`);
-    display.textContent = `${currentCalc}`;
-    calcArr = [];
-    calcArr.push(+currentCalc);
-    addBtnPressed = false;
-    console.log(
-      `currentCalc:${currentCalc}, calcArr[0]:${
-        calcArr[0]
-      }, +display.textContent:${+display.textContent}`
-    );
-
-    
-  }
-  // display.textContent = ``;
+function clearDisplay() {
+  display.textContent = ``;
+  currentValue = ``;
 }
+
+let array = [];
+
+function add() {
+  // array.push(`+`);
+
+  // if (array[array.length - 1] === `+`) {
+  //   console.log(` is + -> array[array.length-1]: ${array[array.length - 1]}`);
+  // }
+
+  if (valueA === 0) {
+    valueA = +currentValue;
+    currentValue = ``;
+    // array.push(`+`);
+  } else if (array[array.length - 1] != `+`) {
+    console.log(
+      ` is not + -> array[array.length-1]: ${array[array.length - 1]}`
+    );
+    console.log(`is not + -> valueA:${valueA}`);
+    console.log(`is not + -> curretnValue:${currentValue}`);
+
+    equals();
+    // array.push(`+`);
+  } else {
+    // display.textContent = ``;
+    valueA += +currentValue;
+    display.textContent = `${valueA}`;
+    currentValue = ``;
+    // array.push(`+`);
+    console.log(`in else + valueA:${valueA}`);
+  }
+  array.push(`+`);
+  console.log(`valueA:${valueA}`);
+}
+
+function sub() {
+  if (valueA === 0) {
+    valueA = +currentValue;
+    currentValue = ``;
+    array.push(`-`);
+  } else if (array[array.length - 1] != `-`) {
+    console.log(
+      ` is not - -> array[array.length-1]: ${array[array.length - 1]}`
+    );
+    console.log(`is not - -> valueA:${valueA}`);
+    console.log(`is not - -> curretnValue:${currentValue}`);
+
+    equals();
+    array.push(`-`);
+  } else {
+    // display.textContent = ``;
+    valueA -= +currentValue;
+    display.textContent = `${valueA}`;
+    currentValue = ``;
+    array.push(`-`);
+    console.log(`in else - valueA:${valueA}`);
+  }
+
+  console.log(`valueA:${valueA}`);
+}
+
+function mul() {}
+
+function div() {}
+
+function mod() {}
+
+function equals() {
+  switch (array[array.length - 1]) {
+    case `+`:
+      add();
+      break;
+    case `-`:
+      sub();
+      break;
+    case `*`:
+      mul();
+      break;
+    case `/`:
+      div();
+      break;
+    case `%`:
+      mod();
+      break;
+    default:
+      console.log(`nothing ran`);
+  }
+  array = [];
+  return (equalWasPressed = false);
+}
+
 function equalBtnPressed() {
   if (addBtnPressed === true) {
     console.log(
@@ -123,16 +203,19 @@ function equalBtnPressed() {
 
 function delBtn() {
   display.textContent = ``;
-  text = ``;
+  currentValue = ``;
+  //only if they hit 10 digits
   blockAllNumBtn(false);
 }
 
 function acBtn() {
   display.textContent = ``;
-  text = ``;
+  currentValue = ``;
   blockAllNumBtn(false);
   // reset add array
   calcArr = [];
+
+  valueA = 0;
 }
 
 function blockAllNumBtn(boolean) {
