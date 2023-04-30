cc4b37dd0caf3a6772bbe6f721d0bbf683c27b25
gustavords <gustavords92@gmail.com>
2023-04-30 00:36:19 -0400

    Add css-colour-scheme, modulus function, sign-operator, and decimal operator

--- js/script_2023.js
+++ js/script_2023.js
@@ -7,6 +7,8 @@ const opr = document.getElementsByClassName(`opr`);
 const history = document.getElementById(`history`);
 const clearBtn = document.getElementById(`oprClr`);
 const delBtn = document.getElementById(`oprDel`);
+const deciBtn = document.getElementById(`oprDeci`);
+const signBtn = document.getElementById(`oprSign`);
 
 let displayHistory = ``;
 let pressed = ``;
@@ -28,9 +30,12 @@ const multiply = (a, b) => {
 	return a * b;
 };
 const divide = (a, b) => {
+	if (a === 0 && b === 0) return `you can't do that silly, error`;
 	return a / b;
 };
-
+const modulus = (a, b) => {
+	return a % b;
+};
 
 /**
  * TODO: make history work for multiple numbers pressed
@@ -38,7 +43,6 @@ const divide = (a, b) => {
 //event for displays pressed numbers
 for (let button of numbers) {
 	button.addEventListener(`click`, () => {
-		
 		//removes text
 		text.textContent = ``;
 		//creates a string with all numbers pressed
@@ -48,8 +52,7 @@ for (let button of numbers) {
 		//re-enables operation buttons
 
 		//might go
-		history.textContent += pressed + ` `;
-
+		// history.textContent += pressed + ` `;
 
 		disableBtn(opr, false);
 	});
@@ -58,11 +61,14 @@ for (let button of numbers) {
 //does all operations
 /**
  * TODO:
- * possibly place most of it into a calculate function,
+ * -possibly place most of it into a calculate function,
  * and just let the event focus on the display
+ * - fix the . and operator sequence issue that returns NaN
  */
 for (let button of operations) {
 	button.addEventListener(`click`, () => {
+		deciBtn.disabled = false;
+
 		//clears pressed buttons
 		pressed = ``;
 
@@ -79,28 +85,35 @@ for (let button of operations) {
 			button.textContent == `+` ||
 			button.textContent == `-` ||
 			button.textContent == `*` ||
-			button.textContent == `/`
+			button.textContent == `/` ||
+			button.textContent == `%`
 		) {
 			calcArr.push(button.textContent);
 			disableBtn(opr, true);
 		}
 
 		calculate();
-		
 
 		//appends to history display
 		//might come off
-		if (
-			button.textContent == `+` ||
-			button.textContent == `-` ||
-			button.textContent == `*` ||
-			button.textContent == `/`
-		) {
-		history.textContent += button.textContent + ` `;
-		}
-		
+		// if (
+		// 	button.textContent == `+` ||
+		// 	button.textContent == `-` ||
+		// 	button.textContent == `*` ||
+		// 	button.textContent == `/`
+		// ) {
+		// history.textContent += button.textContent + ` `;
+		// }
 	});
 }
+signBtn.onclick = () => {
+	// +text.textContent > 0 ? +text.textContent * -1 : +text.textContent * 1;
+	text.textContent = +text.textContent * -1;
+};
+
+deciBtn.onclick = () => {
+	deciBtn.disabled = true;
+};
 
 clearBtn.onclick = () => {
 	history.textContent = ``;
@@ -109,9 +122,9 @@ clearBtn.onclick = () => {
 
 delBtn.onclick = () => {
 	text.textContent = ``;
-	history.textContent += `, `;
+	//mightgo
+	// history.textContent += `, `;
 };
-
 
 function disableBtn(collection, boolean) {
 	if (boolean) {
@@ -138,20 +151,21 @@ function operate(firstNum, operator, secondNum) {
 			return multiply(firstNum, secondNum);
 		case `/`:
 			return divide(firstNum, secondNum);
+		case `%`:
+			return modulus(firstNum, secondNum);
 		default:
 			return `error`;
 	}
 }
 
 function calculate() {
-
 	///equals only works since this is always called in the event
 	if (calcArr.length > 2) {
 		calcArr.push(operate(calcArr[0], calcArr[1], calcArr[2]));
 		calcArr.splice(0, 3);
 		calcArr.reverse();
 		text.textContent = calcArr[0];
-		history.textContent += `= ` + calcArr[0] + ` `;
+		// history.textContent += `= ` + calcArr[0] + ` `;
 		// console.log(calcArr);
 	}
 }
